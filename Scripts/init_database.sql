213a4e349e44795f742516e36805fbc2f987248d
Aawaizp <aawaizpatel1@gmail.com>
2026-01-09 17:36:58 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -3,14 +3,16 @@
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
+    This script creates a new database named 'DataWarehouse' in PgAdimn. 
+    Additionally, the script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+
+1. create Database using query tool in PgAdmin  
+2. create schema 
 */
 
 
-create database Datawarehouse
-use datawarehouse
 create schema bronze
+
 create schema silver
-create schema gold
+
+create schema gold 
